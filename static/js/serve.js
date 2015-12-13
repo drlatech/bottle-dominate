d5b837e4295c420b8ddabc84dfeaad37ebe64c96
Drazen Lazarevic <drazen.lazarevic987@gmail.com>
2015-12-14 00:56:44 +0100

    Ticket #1 - Adding notification plugin CSS and JS files, modifying serve.js file for calling pNotify plugin, slightly modifying python code to add more examples and test with various CSV files. Found issue with pdfkit in printing tables which fits to more pages.

--- static/js/serve.js
+++ static/js/serve.js
@@ -1,10 +1,33 @@
-$(document).ready(function(){  
+$().ready(function(){
     $('#btnSend').click(function(){
          $.ajax({
              type: 'POST',
              url: '/pdf',
-             success: function(data){
-                  alert(data); 
+             data: $('#export').html(),
+             dataType: 'text',
+             success: function(ret){
+                  if (ret == 'OK'){
+
+                    var notice = new PNotify({
+                        title: 'Notification',
+                        text: 'PDF is generated successfully!',
+                        type: 'success',
+                        buttons: {
+                            closer: false,
+                            sticker: false
+                        }
+                    });
+                    notice.get().click(function() {
+                        notice.remove();
+                    });
+                  }
+                  else{
+                    new PNotify({
+                                title: 'Notification',
+                                text: 'Some error occurred during PDF generation!',
+                                type: 'error'
+                    });                  }
+
             } 
         }); 
     });

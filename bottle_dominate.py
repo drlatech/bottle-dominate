059e5e2e819e6f234059da77b0f91de4c6b33428
Drazen Lazarevic <drazen.lazarevic987@gmail.com>
2015-12-17 10:55:42 +0100

    Ticket #7: Added base template view, modified all other views to use this base template view. Added functionality for handling with PDFs. Storing them on server and returning them as response to client in form of inline response that is shown in browser or as attachment. Added new method show_pdf for sending PDF response to the JS. Added functionality in JS for handling PDF files.

--- bottle_dominate.py
+++ bottle_dominate.py
@@ -1,58 +1,82 @@
 from dominate.tags import *
-from bottle import run, route, static_file, get, request, html_escape
 import dominate
-import pdfkit
+from bottle import run, route, static_file, get, request, response
 import csv
 from time import time
+import os
+# PDF manipulation libraries
+import pdfkit
 
 
 @route('/')
 def index():
+    """
+    Function for returning index page view (home page).
+    :return: html: str - generated html
+    """
     return index_page()
 
 
 @route('/premier')
 def premier():
+    """
+    Function for returning premier page view.
+    :return: html: str - generated html
+    """
     return premier_page()
 
 
 @route('/invoice')
 def invoice():
+    """
+    Function for returning invoice page view.
+    :return: html: str - generated html
+    """
     return invoice_page()
 
 
-def index_page():
+def base_page():
+    """
+    Basic template for all pages.
+    It includes all headers, styles, footer and other stuff.
+    It will be passed to other specific views which will add to DOM, things that every of these views show.
+    :return: object of <dominate.document> class
+    """
     doc = dominate.document(title='Home Page')
     with doc.head:
         # CSS
         link(rel='stylesheet', href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css')
+        link(rel='stylesheet', href='jquery.pnotify.default.css')
 
         # JS
         script(type='text/javascript', src='https://code.jquery.com/jquery-1.11.3.min.js')
         script(type='text/javascript', src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js')
+        script(type='text/javascript', src='jquery.pnotify.min.js')
+        script(type='text/javascript', src='serve.js')
+    return doc
+
+
+def index_page():
+    """Function for generating index HTML page
+       :return: HTML page
+    """
+    doc = base_page()
     with doc.body:
         h1('Bottle Dominate PDF Playground')
         with div(style='margin-top:60px'):
-            a('Football', cls='btn btn-default', href='/premier')
-            a('Invoice', cls='btn btn-default', href='/invoice')
+            # a('Football', cls='btn btn-default', href='/premier')
+            # a('Invoice', cls='btn btn-default', href='/invoice')
+            input(type='button', cls= 'btn btn-info', id='premier', value='Football')
+            input(type='button', cls= 'btn btn-info', id='invoice', value='Invoice')
 
     return doc.render()
 
 
 def premier_page():
-    """Function for generating HTML page through Python code."""
-    
-    doc = dominate.document(title='Premier League Table')
-    with doc.head:
-        # JS <scripts></script>
-        script(type='text/javascript', src='https://code.jquery.com/jquery-1.11.3.min.js')
-        script(type='text/javascript', src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js')
-        script(type='text/javascript', src='pnotify.custom.min.js')
-        script(type='text/javascript', src='serve.js')
-
-        # CSS <link>
-        link(rel='stylesheet', href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css')
-        link(rel='stylesheet', href='pnotify.custom.min.css')
+    """Function for generating premier HTML page
+       :return: HTML page
+    """
+    doc = base_page()
     with doc.body:
         h1('Premier League Championship 2015')
         with div(id='content', cls='panel panel-default'):
@@ -74,23 +98,16 @@ def premier_page():
     
 
 def invoice_page():
-    doc = dominate.document(title='List of invoices')
-    with doc.head:
-        # JS <scripts></script>
-        script(type='text/javascript', src='https://code.jquery.com/jquery-1.11.3.min.js')
-        script(type='text/javascript', src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js')
-        script(type='text/javascript', src='pnotify.custom.min.js')
-        script(type='text/javascript', src='serve.js')
-
-        # CSS <link>
-        link(rel='stylesheet', href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css')
-        link(rel='stylesheet', href='pnotify.custom.min.css')
-
+    """
+    Function for generating invoice HTML page
+    :return: HTML page
+    """
+    doc = base_page()
     with doc.body:
         h1('Invoice List')
         with div(id='content', cls='panel panel-default'):
             with table(cls='table', id='export'):
-                with open('invoice.csv', 'r') as f:
+                with open('table.csv', 'r') as f:
                     data = [row for row in csv.reader(f.read().splitlines())]
                 with thead():
                     with tr():
@@ -108,14 +125,28 @@ def invoice_page():
 
 @route('/pdf', method='POST')
 def pdf_it():
+    """
+    Function for generating PDF from HTML and some options which are defined in this function. Function is taking AJAX
+    POST data that represents html <table> generated in view and append it in <body> block.
+    :return: name: str - name of generated PDF file
+    """
     if request.is_ajax:
+        # POST data
         data = request.body.read()
-        options = {'page-size':'A4'}
-        page = dominate.document()
+        # Options of PDF
+        options = {'page-size': 'A4',
+                   'orientation': 'Landscape'
+        }
+        page = dominate.document('PDF Export')
         with page.head:
             # CSS
             style("""td, th{text-align: left;}
-                     table{width: 100%;}""")
+                     table{width: 100%;}
+                     tr{
+                        page-break-inside: avoid;
+                     }
+                  """
+            )
             # JS
             script(type='text/javascript', src='https://code.jquery.com/jquery-1.11.3.min.js')
             script(
@@ -126,13 +157,36 @@ def pdf_it():
                 });
 
                 """, type='text/javascript')
-        page.body.add(h1('Premier League Championship 2015'))
+
         page.body.add_raw_string(data)
         name = 'export_' + str(time()) + '.pdf'
         page = page.render()
-        print page
-        pdfkit.from_string(page, name, options=options)
-        return 'OK'
+        # Change directory to tmp for storing PDFs.
+        try:
+            os.mkdir('tmp')
+        except OSError:
+            # Passing exceptions is really bad habit, but in this case it is not necessary to process it, because it is
+            # raised if folder already exists, so this doesn't mean anything, and checking for folder existing with
+            # trying to make it is more important thing.
+            pass
+        path = 'tmp/' + name
+        pdfkit.from_string(page, path, options=options)
+        return name
+
+
+@route('/showpdf/<name>')
+def show_pdf(name):
+    """
+    Function for returning PDF file as response which is generated in pdf_it function. It is necessary to setup headers
+     from response to make it appropriate PDF response.
+    :param name: str - name of the generated PDF that should be shown inline or as a attachment.
+    :return: PDF file
+    """
+    response.set_header('Content-type', 'application/pdf')
+    response.set_header('Content-Disposition', 'inline;filename={}'.format(name))
+    path = 'tmp/' + name
+    pdf = file(path, 'rb')
+    return pdf
 
 
 @get('/<filename:re:.*\.js>')
@@ -152,4 +206,4 @@ def csss(filename):
 
 
 if __name__ == '__main__':
-    run(host='localhost', port=4000, debug=True, reloader=True)
+    run(host='localhost', port=4000, debug=True, reloader=True, server='diesel')
--- static/js/serve.js
+++ static/js/serve.js
@@ -5,33 +5,44 @@ $().ready(function(){
              url: '/pdf',
              data: $('#export').get(0).outerHTML,//.html(),
              dataType: 'html',
-             success: function(ret){
-                  if (ret == 'OK'){
-
-                    //var notice = new PNotify({
+             success: function(data){
+                    window.open('/showpdf/'+data, '_blank');
                     $.pnotify({
                         title: 'Notification',
                         text: 'PDF is generated successfully!',
                         type: 'success',
                         icon: 'ui-icon ui-icon-check'
-
                     });
-                    //notice.get().click(function() {
-                    //    notice.remove();
-                    //});
-                  }
-                  else{
-                    //new PNotify({
+             },
+             error: function(){
                     $.pnotify({
-                                title: 'Notification',
-                                text: 'Some error occurred during PDF generation!',
-                                type: 'error',
-                                icon: 'ui-icon ui-icon-check'
-
+                        title: 'Notification',
+                        text: 'Some error occurred during PDF generation!',
+                        type: 'error',
+                        icon: 'ui-icon ui-icon-check'
                     });
-                  }
+             }
+        });
+    });
+    $('#premier').click(function(){
+        $.ajax({
+            url: '/premier',
+            method: 'GET',
+            dataType: 'html'
+        }).success(function(data){
+            document.write(data);
+            document.close();
+        });
+    });
 
-            } 
-        }); 
+    $('#invoice').click(function(){
+        $.ajax({
+            url: '/invoice',
+            method: 'GET',
+            dataType: 'html'
+        }).success(function(data){
+            document.write(data);
+            document.close();
+        });
     });
 });
